3051bcd1d1a79298aa1ce0d789d705ad0496d1eb
Olha75 <137830839+Olha75@users.noreply.github.com>
2023-10-28 23:47:36 +0300

    1 z

---
+++ js/task-01.js
@@ -0,0 +1,9 @@
+const itemElement = document.querySelectorAll("#categories .item");
+console.log(`Number of categories: ${itemElement.length}`);
+itemElement.forEach((elem, i) => {
+    const title = elem.querySelector("h2");
+    console.log("");
+    console.dir(`Category: ${title.textContent}`);
+    const liItemElement = elem.querySelectorAll("li");
+    console.dir(`Elements: ${liItemElement.length}`);
+});
